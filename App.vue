8c7368dd1d89dd5a197814feca9d06fe5318231b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-10 18:02:06 +0000

    Deploy avto-dev/bank-card-vue-component to github.com/avto-dev/bank-card-vue-component.git:gh-pages

---
+++ App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <!--<Card is-new is-small />-->
+    <!--<Card is-new is-small :errors="errors" />-->
+    <!--<Card :number="number" />-->
+    <VueBankCard is-new :is-reset="reset" @reset="reset = $event" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      number: "4276163199737755",
+      errors: {
+        cardNumber: "Недостаточно средств на карте",
+        expDateMonth: "Карта просрочена",
+        cvv: "Неверный код cvv"
+      },
+      reset: false
+    };
+  },
+  methods: {
+    onEnter(event) {
+      event.preventDefault();
+      console.log(event);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
+
+#app {
+  width: 420px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
---
+++ index.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import VueBankCard from "../src/index";
+import App from "./App";
+
+Vue.component("VueBankCard", VueBankCard);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
