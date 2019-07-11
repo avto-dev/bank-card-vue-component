1f28c28ccba4cf81278983184d504212196231b7
Georgy Nikolaev <nikolaev.g@avtocod.ru>
2019-07-11 14:45:16 +0500

    Added cross-env, update demo app page

--- demo/App.vue
+++ demo/App.vue
@@ -1,15 +1,42 @@
 <template>
     <div id="app">
-        <VueBankCard number="5536 9111 2222 3333"/>
+        <h1>Bank card vue component</h1>
+        <h2>v{{ pckg.version }}</h2>
+
+        <ul>
+            <li>
+                <h3>New base card</h3>
+                <VueBankCard is-new />
+            </li>
+            <li>
+                <h3>Saved base card</h3>
+                <VueBankCard :number="number" />
+            </li>
+            <li>
+                <h3>New small card</h3>
+                <VueBankCard is-new is-small />
+            </li>
+            <li>
+                <h3>Saved small card</h3>
+                <VueBankCard is-small :number="number" />
+            </li>
+        </ul>
+
+        <p>Docs are available by <a :href="readmePath">link</a></p>
     </div>
 </template>
 
 <script>
+import pjson from "../package";
+
 export default {
     name: "App",
     data() {
         return {
-            number: "4276163199737755",
+            pckg: pjson,
+            readmePath:
+                "https://github.com/avto-dev/bank-card-vue-component/blob/dev/README.md",
+            number: "5536 9111 2222 3333",
             errors: {
                 cardNumber: "Недостаточно средств на карте",
                 expDateMonth: "Карта просрочена",
@@ -21,7 +48,6 @@ export default {
     methods: {
         onEnter(event) {
             event.preventDefault();
-            console.log(event);
         }
     }
 };
@@ -34,8 +60,22 @@ export default {
     box-sizing: border-box;
 }
 
+body {
+    font-family: sans-serif;
+}
+
+ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+}
+
+ul li {
+    margin-bottom: 40px;
+}
+
 #app {
-    width: 420px;
+    max-width: 600px;
     display: flex;
     flex-direction: column;
     align-items: center;
